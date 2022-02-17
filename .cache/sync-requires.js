47508b07279418ab2475bc4989b3b41f73aea8d6
Jonas Wagner <37350002+jonaswagnr@users.noreply.github.com>
2022-02-17 10:29:50 +0100

    speisekartenupdate

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,10 +5,10 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/jonas/Documents/GitHub/WEB-Blome/.cache/dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("/Users/jonas/Documents/GitHub/WEB-Blome/src/pages/404.js"))),
-  "component---src-pages-datenschutz-js": hot(preferDefault(require("/Users/jonas/Documents/GitHub/WEB-Blome/src/pages/datenschutz.js"))),
-  "component---src-pages-impressum-js": hot(preferDefault(require("/Users/jonas/Documents/GitHub/WEB-Blome/src/pages/impressum.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/jonas/Documents/GitHub/WEB-Blome/src/pages/index.js")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/jonaswagner/Documents/GitHub/WEB-Blome/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/jonaswagner/Documents/GitHub/WEB-Blome/src/pages/404.js"))),
+  "component---src-pages-datenschutz-js": hot(preferDefault(require("/Users/jonaswagner/Documents/GitHub/WEB-Blome/src/pages/datenschutz.js"))),
+  "component---src-pages-impressum-js": hot(preferDefault(require("/Users/jonaswagner/Documents/GitHub/WEB-Blome/src/pages/impressum.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/jonaswagner/Documents/GitHub/WEB-Blome/src/pages/index.js")))
 }
 
